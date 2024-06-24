709f2e0007aa604b377b8bf7b201332a3760d501
b0nsky <165460197+b0nsky@users.noreply.github.com>
2024-06-24 10:24:50 +0700

    Update essay4.cpp

---
+++ UTS-BahasaPemrograman/essay4.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int usia;
+    std::cout << "Masukkan Usia Kalian: ";
+    std::cin >> usia;
+    std::cout << "Berarti Usia Kalian sekarang adalah " << usia << " Tahun" << std::endl;
+    return 0;
+}
